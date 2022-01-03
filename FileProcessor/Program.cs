ca2f48164f4f554519a2356c28cf1a54792335ff
ssakib005 <sshadman005@gmail.com>
2022-01-03 22:29:03 +0600

    File Processor

--- FileProcessor/Program.cs
+++ FileProcessor/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace FileProcessor
 {
@@ -10,6 +7,38 @@ namespace FileProcessor
     {
         static void Main(string[] args)
         {
+            Start(@"your file path");
+        }
+        public static void Start(string path)
+        {
+            if (File.Exists(path))
+            {
+                //File Path
+                ProcessFile(path);
+            }
+            else if (Directory.Exists(path))
+            {
+                // Directory Path
+                ProcessDirectory(path);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a valid file or directory.", path);
+            }
+        }
+        static void ProcessDirectory(string targetDirectory)
+        {
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+                ProcessFile(fileName);
+
+            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach (string subdirectory in subdirectoryEntries)
+                ProcessDirectory(subdirectory);
+        }
+        static void ProcessFile(string path)
+        {
+            Console.WriteLine("Processed file '{0}'.", path);
         }
     }
 }
